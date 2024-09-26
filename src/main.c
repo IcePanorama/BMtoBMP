b459cad916d3a5fddf4c8deaaa94d537ad572e2a
IcePanorama <thriftedroot@protonmail.com>
2024-09-26 13:18:51 -0400

    Implemented loading of BM/PAL files.

--- src/main.c
+++ src/main.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+FILE *load_file (const char *filename);
 
 int
 main (void)
 {
-  puts ("Hello world!");
+  const char *bm_filename = "AUTOGRPH.BM";
+  const char *pal_filename = "AUTOGRPH.PAL";
+
+  FILE *bm_file = load_file (bm_filename);
+  FILE *pal_file = load_file (pal_filename);
+
+  fclose (bm_file);
+  fclose (pal_file);
+
   return 0;
+}
+
+FILE *
+load_file (const char *filename)
+{
+  FILE *fptr = fopen (filename, "rb");
+  if (fptr == NULL)
+    {
+      fprintf (stderr, "Error: unable to open file, %s.\n", filename);
+      exit (1);
+    }
+
+  return fptr;
 }
