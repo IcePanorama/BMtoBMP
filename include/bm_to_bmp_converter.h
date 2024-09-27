30f751a5bdfd609c56e6c33ca3729e2e37c6e27b
IcePanorama <thriftedroot@protonmail.com>
2024-09-27 14:56:23 -0400

    Finally fixed itgit add .git add .git add .!

--- include/bm_to_bmp_converter.h
+++ include/bm_to_bmp_converter.h
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 #define BMtoBMP_BYTES_PER_PIXEL (3) // 24-bits-per-pixel
 #define BMtoBMP_OUTPUT_FILENAME_MAX_LEN (256)
@@ -12,11 +13,58 @@
 
 typedef struct BMtoBMP_BitmapImage_s
 {
+  char filename[BMtoBMP_OUTPUT_FILENAME_MAX_LEN];
   uint32_t width;
   uint32_t height;
-  uint32_t row_size;
-  uint8_t *data;
+  uint8_t **data;
 } BMtoBMP_BitmapImage_t;
+
+static void
+allocate_img_data (BMtoBMP_BitmapImage_t *img)
+{
+  img->data = (uint8_t **)calloc (img->height, sizeof (uint8_t *));
+  if (img->data == NULL)
+    {
+      fprintf (stderr,
+               "[BMtoBMP] calloc error: unable to allocate image buffer of "
+               "size, %dx%dx%d.\n",
+               img->height, img->width, BMtoBMP_BYTES_PER_PIXEL * 8);
+      return;
+    }
+
+  for (uint32_t i = 0; i < img->height; i++)
+    {
+      img->data[i] = (uint8_t *)calloc (img->width * BMtoBMP_BYTES_PER_PIXEL,
+                                        sizeof (uint8_t));
+      if (img->data[i] == NULL)
+        {
+          for (uint32_t j = 0; j < i; j++)
+            {
+              free (img->data[j]);
+            }
+          free (img->data);
+          img->data = NULL;
+          return;
+        }
+    }
+}
+
+static void
+destroy_img_data (BMtoBMP_BitmapImage_t *img)
+{
+  if (img == NULL)
+    return;
+
+  if (img->data != NULL)
+    {
+      for (uint32_t i = 0; i < img->height; i++)
+        {
+          free (img->data[i]);
+        }
+      free (img->data);
+      img->data = NULL;
+    }
+}
 
 static int8_t
 read_uint32_from_file (FILE *fptr, uint32_t *output)
@@ -108,26 +156,12 @@ write_string_to_file (FILE *fptr, const char *s, size_t s_len)
 static int8_t
 process_image (FILE *bm_file, FILE *pal_file, BMtoBMP_BitmapImage_t *img)
 {
-  if (read_uint32_from_file (bm_file, &img->width) != 0
-      || read_uint32_from_file (bm_file, &img->height) != 0)
-    {
-      return -1;
-    }
+  fseek (bm_file, 0x0C, SEEK_SET);
 
-  img->row_size = ((img->width * 3 + 3) & ~3);
-  img->data = calloc (img->row_size * img->height, sizeof (uint8_t));
-  if (img->data == NULL)
+  // for (uint32_t i = 0; i < img->height; i++)
+  for (int32_t i = img->height - 1; i >= 0; i--)
     {
-      fprintf (stderr,
-               "[BMtoBMP] calloc error: unable to allocate image buffer of "
-               "size, %dx%dx%d.\n",
-               img->height, img->width, BMtoBMP_BYTES_PER_PIXEL * 8);
-      goto clean_up;
-    }
-
-  for (uint32_t i = 0x0; i < img->height; i++)
-    {
-      for (uint32_t j = 0x0; j < img->width; j++)
+      for (uint32_t j = 0; j < img->width; j++)
         {
           /* Get PAL offset from BM file. */
           uint8_t offset;
@@ -137,7 +171,7 @@ process_image (FILE *bm_file, FILE *pal_file, BMtoBMP_BitmapImage_t *img)
               fprintf (
                   stderr,
                   "[BMtoBMP] fread error: error reading data from BM file.\n");
-              goto clean_up;
+              return -1;
             }
 
           /* Read color data from PAL file using offset. */
@@ -151,72 +185,72 @@ process_image (FILE *bm_file, FILE *pal_file, BMtoBMP_BitmapImage_t *img)
                 {
                   fprintf (stderr, "[BMtoBMP] fread error: error reading data "
                                    "from PAL file.\n");
-                  goto clean_up;
+                  return -1;
                 }
             }
 
-          const uint32_t i_reversed = (img->height - 1 - i);
-
           /* Data must be in LE order so it actually goes BGR, not RGB. */
-          img->data[(i_reversed * img->row_size) + (j * 3)] = color_data[2];
-          img->data[(i_reversed * img->row_size) + (j * 3) + 1]
-              = color_data[1];
-          img->data[(i_reversed * img->row_size) + (j * 3) + 2]
-              = color_data[0];
+          img->data[i][j * 3] = color_data[2];
+          img->data[i][j * 3 + 1] = color_data[1];
+          img->data[i][j * 3 + 2] = color_data[0];
         }
-
-      // Skip padding after reading each row
-      fseek (bm_file, img->row_size - img->width * 3, SEEK_CUR);
-
-      // printf ("%ld\n", ftell (bm_file));
     }
 
   return 0;
-clean_up:
-  free (img->data);
-  return -1;
+}
+
+static int8_t
+create_image (FILE *bm_file, BMtoBMP_BitmapImage_t *img)
+{
+  if (read_uint32_from_file (bm_file, &img->width) != 0
+      || read_uint32_from_file (bm_file, &img->height) != 0)
+    {
+      return -1;
+    }
+
+  allocate_img_data (img);
+  if (img->data == NULL)
+    return -1;
+
+  return 0;
 }
 
 int8_t
 BMtoBMP_convert_image (FILE *bm_file, FILE *pal_file,
                        char const output_filename[static 1])
 {
-  BMtoBMP_BitmapImage_t img;
-  if (process_image (bm_file, pal_file, &img) != 0)
-    return -1;
-
-  if (strlen (output_filename) + strlen (".bmp") + 1
-      > BMtoBMP_OUTPUT_FILENAME_MAX_LEN)
+  /* len(".BMP\0") = 5 */
+  if (strlen (output_filename) + 5 > BMtoBMP_OUTPUT_FILENAME_MAX_LEN)
     {
       fprintf (stderr, "[BMtoBMP] output filename is too long.\n");
-      goto clean_up;
+      return -1;
     }
 
-  char filename[BMtoBMP_OUTPUT_FILENAME_MAX_LEN];
-  strcpy (filename, output_filename);
-  strcat (filename, ".bmp");
+  BMtoBMP_BitmapImage_t img;
+  if (create_image (bm_file, &img) != 0)
+    return -1;
 
-  FILE *output = fopen (filename, "wb");
+  if (process_image (bm_file, pal_file, &img) != 0)
+    goto clean_up;
+
+  strcpy (img.filename, output_filename);
+  strcat (img.filename, ".bmp");
+
+  FILE *output = fopen (img.filename, "wb");
   if (output == NULL)
     {
       fprintf (stderr, "[BMtoBMP] fopen error: could not create file, %s.\n",
-               filename);
+               img.filename);
       goto clean_up;
     }
 
   // Make sure we're at the beginning of the file
   fseek (output, 0x0, SEEK_SET);
 
-  uint32_t pixel_data_size
-      = (img.row_size * img.height * BMtoBMP_BYTES_PER_PIXEL);
-  /*
-  const uint32_t padding = (4 - (pixel_data_size % 4)) % 4;
-  pixel_data_size += padding;
-  */
+  const uint32_t pixel_data_size
+      = (img.width * img.height * BMtoBMP_BYTES_PER_PIXEL);
   const uint32_t output_file_size = 54 + pixel_data_size;
-
-  const int32_t ppm_resolution
-      = (int32_t)(BMtoBMP_DPI * 39.37); // pixel per meter
+  const int32_t ppm_resolution = 0x0B13; // pixel per meter
 
   if (write_string_to_file (output, "BM", 2) != 0 // file signature
       || write_le_int32_to_file (output, output_file_size) != 0
@@ -228,7 +262,7 @@ BMtoBMP_convert_image (FILE *bm_file, FILE *pal_file,
       || write_le_int16_to_file (output, 0x1) != 0 // num color planes
       || write_le_int16_to_file (output, BMtoBMP_BYTES_PER_PIXEL * 8) != 0
       || write_le_int32_to_file (output, 0x0) != 0 // no compression
-      || write_le_int32_to_file (output, pixel_data_size) != 0
+      || write_le_int32_to_file (output, 0x0) != 0
       || write_le_int32_to_file (output, ppm_resolution) // horizontal
       || write_le_int32_to_file (output, ppm_resolution) // vertical
       || write_le_int32_to_file (output, 0x0) // num colors in palette
@@ -239,21 +273,36 @@ BMtoBMP_convert_image (FILE *bm_file, FILE *pal_file,
       goto clean_up;
     }
 
-  // Recalculating pixel data size as padding may have been applied.
-  for (uint32_t i = 0; i < img.row_size * img.height; i++)
+  const uint32_t padding = (4 - (img.width * BMtoBMP_BYTES_PER_PIXEL) % 4) % 4;
+  for (uint32_t i = 0; i < img.height; i++)
     {
-      if (write_le_int8_to_file (output, img.data[i]) != 0)
+      for (uint32_t j = 0; j < img.width; j++)
         {
-          fclose (output);
-          goto clean_up;
+          for (uint32_t k = 0; k < BMtoBMP_BYTES_PER_PIXEL; k++)
+            {
+              if (write_le_int8_to_file (output, img.data[i][j * 3 + k]) != 0)
+                {
+                  fclose (output);
+                  goto clean_up;
+                }
+            }
+        }
+      for (uint32_t p = 0; p < padding; p++)
+        {
+          write_le_int8_to_file (output, 0x0);
         }
     }
 
+  /* Update file size in header. */
+  size_t size = ftell (output);
+  fseek (output, 0x22, SEEK_SET);
+  write_le_int32_to_file (output, size - 54);
+
+  destroy_img_data (&img);
   fclose (output);
-  free (img.data);
   return 0;
 clean_up:
-  free (img.data);
+  destroy_img_data (&img);
   return -1;
 }
 
